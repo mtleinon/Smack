2782e068e57894200087b3cca67b3138623f3d02
mtleinon <mika.t.leinonen@gmail.com>
2017-10-16 13:44:20 +0300

    Added main view.

--- app/src/main/java/com/example/mikat/smack/MainActivity.kt
+++ app/src/main/java/com/example/mikat/smack/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.mikat.smack
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.content_main.*
@@ -25,7 +22,9 @@ class MainActivity : AppCompatActivity() {
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
-        loginBtnNavHeader.setOnClickListener {
+        loginBtnNav.setOnClickListener {
+            val loginIntent = Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
 
         }
 
